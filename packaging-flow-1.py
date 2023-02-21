8836a665aaa6982778945b70a3c0464d44b9496b
Andrea Tentner <tentner-andrea@norc.org>
2023-02-21 15:35:23 -0600

    finish flow to infer minimal dd from csv data file and create editable heal csv dd from template for investigator to add detail

--- packaging-flow-1.py
+++ packaging-flow-1.py
@@ -3,8 +3,11 @@ from frictionless import describe
 import pandas as pd
 import json
 import requests
+import pipe
 
 ###########################################################################
+# get the latest version of the heal json dd schema to populate the 
+# heal csv dd template
 ###########################################################################
 
 heal_dd_json_schema_url = 'https://raw.githubusercontent.com/HEAL/heal-metadata-schemas/main/variable-level-metadata-schema/schemas/fields.json'
@@ -21,52 +24,39 @@ for p in list(heal_dd_json_schema['properties'].keys()):
     else:
         my_df_col.append(p)
          
-my_df_col
+
+heal_dd_df = pd.DataFrame(columns=my_df_col)
 
 ###########################################################################
+# get the required fields from the heal json dd schema and put those 
+# first in the heal csv dd template
 ###########################################################################
 
+required_col = heal_dd_json_schema['required']
+
+def everything_after(df, cols):
+    another = df.columns.difference(cols, sort=False).tolist()
+    return df[cols + another]
+
+heal_dd_df = heal_dd_df.pipe(everything_after, required_col)
+
+###########################################################################
+# bring in a new tabular resource, infer minimal table schema/dd
+###########################################################################
 
 resource = describe('./package/data-snp-exp-1.csv')
 resource.to_json('./package/data-snp-exp-1.resource.json')
 
-#df = pd.read_json('./package/data-snp-exp-1.resource.json')
-#df.to_csv('./package/my_df.csv')
 with open('./package/data-snp-exp-1.resource.json','r') as f:
     data = json.loads(f.read())
 
 df = pd.json_normalize(data, record_path=['schema','fields'])
-print(df)
-#df.to_csv('./package/my_df.csv',index=False)
 
-heal_dd_df = pd.DataFrame(columns=['name','type','description','title','module','format','encodings','constraints.enum'])
-heal_dd_df
-heal_dd_df['name'] = df['name']
-heal_dd_df
-heal_dd_df['type'] = df['type']
-heal_dd_df
+###########################################################################
+# write inferred dd to the heal csv dd template so that investigator
+# can add information - make a more detailed dd
+###########################################################################
+
+heal_dd_df = pd.concat([heal_dd_df,df])
 heal_dd_df.to_csv('./package/my_heal_dd_df.csv',index=False)
 
-heal_dd_full_df = pd.DataFrame(columns=['name','type','description','title','module','format','encodings','constraints.enum'])
-heal_dd_full_df
-heal_dd_full_df['name'] = df['name']
-heal_dd_full_df
-heal_dd_full_df['type'] = df['type']
-heal_dd_full_df
-heal_dd_full_df.to_csv('./package/my_heal_dd_full_df.csv',index=False)
-
-pprint(resource)
-pprint(type(resource))
-pprint(type(resource.schema.fields))
-pprint(type(resource.schema.fields[0]))
-pprint(type(json.dumps(resource.schema.fields[0])))
-
-#pprint(pd.DataFrame(resource.schema.fields))
-#pprint(pd.json_normalize(resource, record_path='fields'))
-
-#pprint(pd.DataFrame.from_dict(resource.schema.fields))
-#pprint(pd.DataFrame.from_records(resource.schema.fields))
-
-df = pd.DataFrame(resource.schema.fields)
-pprint(type(df))
-df.to_csv('./package/my_df.csv')

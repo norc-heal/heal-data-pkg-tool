47e136757fbfcfdab943c16141618bee5850de06
Andrea Tentner <tentner-andrea@norc.org>
2023-04-26 18:57:31 -0500

    got experiment tracker form working but still need to figure out how to save input values to file

--- test-form-gen-3.py
+++ test-form-gen-3.py
@@ -13,31 +13,37 @@ if __name__ == "__main__":
         "title": "HEAL Experiment Tracker",
         "properties": {
             "experiment.id": {
-                "label": "Experiment ID",
+                "title": "Experiment ID",
                 "description": "id assigned to each experiment relevant to the data package; prefix is 'exp-' followed by a number starting with 1 for the first experiment, and iterating by 1 for each successive experiment (i.e. exp-1, exp-2, etc.)",
                 "type": "string",
                 "pattern": "^exp-+-*[0-9]*[1-9][0-9]*$"
             },
             "experiment.type": {
-                "label" : "Experiment Type",
+                "title" : "Experiment Type",
                 "description": "discovery|materials and methods development",
                 "type": "string",
                 "enum": ["discovery","materials and methods development"]
             },
-            "experiment.question": {
-                "label": "Experiment Question(s)",
-                "description": "what question(s) does the experimentalist hope to address with this experiment? be as specific as possible",
-                "type": "string"
-            },
             "experiment.description": {
-                "label": "Experiment Description",
+                "title": "Experiment Description",
                 "description": "provide a brief description of the experiment; this is NOT a protocol",
                 "type": "string"
             },
+            "experiment.question": {
+                "title": "Experiment Question(s)",
+                "description": "what question(s) does the experimentalist hope to address with this experiment? be as specific as possible",
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
             "experiment.hypothesis": {
-                "label": "Experiment Hypothesis(es)",
+                "title": "Experiment Hypothesis(es)",
                 "description": "for each question the experimentalist hopes to address with this experiment, what does the experimentalist hypothesize will be the result(s) of the experiment? Be as specific as possible",
-                "type": "string"
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
             }
         }
     }

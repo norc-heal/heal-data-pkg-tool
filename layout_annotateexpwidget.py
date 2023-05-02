23bc61c122ddcc13359fb7b966406fa903d059b7
Andrea Tentner <tentner-andrea@norc.org>
2023-05-02 15:30:38 -0500

    finished implementing check if exp file already exists prior to writing to file - also implemented status message to let user know if save was successful and next steps or if not successful why and some hints on next steps

--- layout_annotateexpwidget.py
+++ layout_annotateexpwidget.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from json import dumps, loads
 
 from qtpy import QtWidgets
@@ -47,10 +48,15 @@ class AnnotateExpWindow(QtWidgets.QWidget):
         # create save button
         self.buttonSaveExp = QtWidgets.QPushButton(text="Save experiment",parent=self)
         self.buttonSaveExp.clicked.connect(self.save_exp)
+
+        # create status message box
+        self.userMessageBox = QtWidgets.QTextEdit(parent=self)
+        self.userMessageBox.setReadOnly(True)
         
-        # add form and save button
+        # add form and save button and status message box
         layout.addWidget(self.form)
         layout.addWidget(self.buttonSaveExp)
+        layout.addWidget(self.userMessageBox)
 
         #self.form.widget.on_changed.connect(self.validate_exp_id)
         #form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4)))
@@ -64,13 +70,25 @@ class AnnotateExpWindow(QtWidgets.QWidget):
         #self.form.widget(lambda d: print(dumps(d, indent=4), file=open('test-out-'+ loads(dumps(d, indent=4))['experiment.id'] + '.txt','w')))
         print(self.form.widget.state)
         exp = self.form.widget.state
-        print(exp["experiment.id"])
-        print(dumps(exp, indent=4), file=open('exp-trk-'+ exp["experiment.id"] + '.txt','w'))
-        #print(self.form[0])
-        #print(qt_object_properties(self.form))
+        exp_id = exp["experiment.id"]
+        print(exp_id)
 
-        #for widget in self.form:
-        #    print(widget)
+        saveFolderPath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Your DSC Data Package Directory - Your new experiment will be saved there!')
+        
+        expFileName = 'exp-trk-'+ exp_id + '.txt'
+        saveFilePath = os.path.join(saveFolderPath,expFileName)
+        
+        messageText = ""
+        
+        if os.path.isfile(saveFilePath):
+            messageText = "An experiment file for an experiment with id " + exp_id + " already exists at " + saveFilePath + ". You may want to do one or both of: 1) Use the View/Edit tab to view your experiment tracker file and check which experiment IDs you've already used and added to your tracker, 2) Use File Explorer to navigate to your DSC Data Package Directory and check which experiment IDs you've already used and for which you've already created experiment files - these files will be called \'exp-trk-exp-{a number}.txt\'. While you perform these checks, your experiment tracker form will remain open unless you explicitly close it. You can come back to it, change your experiment ID, and hit the save button again to save with an experiment ID that is not already in use. If you meant to overwrite an experiment file you previously created for an experiment with this experiment ID, please delete the previously created experiment file and try saving again." 
+        else:
+            print(dumps(exp, indent=4), file=open(saveFilePath,'w'))
+            messageText = "Your experiment file was successfully written at: " + saveFilePath + ". You'll want to head back to the \'Add Experiment\' tab and use the \'Add Experiment\' button to add this experiment file to your experiment tracker file! You can do this now, or later - You can add experiment files to the experiment tracker file one at a time, or you can add multiple experiment files all at once, so you may choose to create experiment files for all of your experiments and then add them in one go to your experiment tracker file."
+        
+        self.userMessageBox.setText(messageText)
+        
+        
 
 if __name__ == "__main__":
     #app = QtWidgets.QApplication(sys.argv)

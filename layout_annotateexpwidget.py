2bd45e7837f86b63fdedecd624aa1bd4dc353b81
Andrea Tentner <tentner-andrea@norc.org>
2023-05-02 14:27:56 -0500

    mods to exp tracker gui form widget to add save button and only save when save button is clicked - instead of whenever the form changes - contents of exp tracker form will be saved as a json file with file name exp-trk-{exp-id}.txt - started to implement a check prior to save that no file with that name currently exists - this will be a first check to ensure that the user has not already named an experiment with the current exp-id, though there will need to be a second check before adding to the exp tracker file to ensure that an exp with current exp id wasn't added manually or added via this txt file approach but the user deleted the source text file after the addition to the tracker file

--- layout_annotateexpwidget.py
+++ layout_annotateexpwidget.py
@@ -6,6 +6,7 @@ from qtpy import QtWidgets
 from qt_jsonschema_form import WidgetBuilder
 
 from schema_experiment_tracker import schema_experiment_tracker
+from dsc_pkg_utils import qt_object_properties
 import pandas as pd
 
 class AnnotateExpWindow(QtWidgets.QWidget):
@@ -19,8 +20,6 @@ class AnnotateExpWindow(QtWidgets.QWidget):
         self.setLayout(layout)
         
         # Create the form widget 
-           
-
         builder = WidgetBuilder()
 
         schema = schema_experiment_tracker
@@ -36,21 +35,42 @@ class AnnotateExpWindow(QtWidgets.QWidget):
         #
         #}
 
-        form = builder.create_form(schema, ui_schema)
-        form.widget.state = {
+        self.form = builder.create_form(schema, ui_schema)
+        self.form.widget.state = {
             "experiment.id": "exp-1",
             #"schema_path": "some_file.py",
             #"integerRangeSteps": 60,
             #"sky_colour": "#8f5902"
         }
         #form.show()
-        layout.addWidget(form)
 
+        # create save button
+        self.buttonSaveExp = QtWidgets.QPushButton(text="Save experiment",parent=self)
+        self.buttonSaveExp.clicked.connect(self.save_exp)
+        
+        # add form and save button
+        layout.addWidget(self.form)
+        layout.addWidget(self.buttonSaveExp)
+
+        #self.form.widget.on_changed.connect(self.validate_exp_id)
         #form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4)))
         #form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4), file=open('test-out.txt','w')))
         #form.widget.on_changed.connect(lambda d: print(loads(dumps(d, indent=4))['experiment.id']))
-        form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4), file=open('test-out-'+ loads(dumps(d, indent=4))['experiment.id'] + '.txt','w')))
+        
+        #form.widget.on_changed.connect(lambda d: print(dumps(d, indent=4), file=open('test-out-'+ loads(dumps(d, indent=4))['experiment.id'] + '.txt','w')))
 
+            
+    def save_exp(self):
+        #self.form.widget(lambda d: print(dumps(d, indent=4), file=open('test-out-'+ loads(dumps(d, indent=4))['experiment.id'] + '.txt','w')))
+        print(self.form.widget.state)
+        exp = self.form.widget.state
+        print(exp["experiment.id"])
+        print(dumps(exp, indent=4), file=open('exp-trk-'+ exp["experiment.id"] + '.txt','w'))
+        #print(self.form[0])
+        #print(qt_object_properties(self.form))
+
+        #for widget in self.form:
+        #    print(widget)
 
 if __name__ == "__main__":
     #app = QtWidgets.QApplication(sys.argv)

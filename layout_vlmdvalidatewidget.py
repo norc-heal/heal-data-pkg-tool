54330f2ea8becc6d6889bc6d466261e699dc3da8
Andrea Tentner <tentner-andrea@norc.org>
2023-05-05 16:50:53 -0500

    added a shell for the vlmd validation tab - still need to implement the validation functionality on push button click

---
+++ layout_vlmdvalidatewidget.py
@@ -0,0 +1,53 @@
+import csv, codecs # base python, no pip install needed
+import os # base python, no pip install needed
+ 
+from PyQt5 import QtCore, QtGui, QtWidgets, QtPrintSupport 
+from PyQt5.QtGui import QImage, QPainter
+from PyQt5.QtCore import QFile, Qt
+
+import sys # base python, no pip install needed
+
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.uic import loadUi
+
+from pathlib import Path # base python, no pip install needed
+
+from healdata_utils.cli import convert_to_vlmd
+
+from frictionless import plugins # frictionless already installed as a healdata_utils dependency, no pip install needed
+from frictionless.plugins import remote
+from frictionless import describe
+
+import pandas as pd # pandas already installed as a healdata_utils dependency, no pip install needed
+import json # base python, no pip install needed
+import requests # requests already installed as a healdata_utils dependency, no pip install needed
+import pipe
+
+import dsc_pkg_utils # local module, no pip install needed
+
+class VLMDValidateWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.w = None  # No external window yet.
+        
+        widget = QtWidgets.QWidget()
+        
+        self.buttonValidateHealCsvDd = QtWidgets.QPushButton(text="Validate HEAL CSV Data Dictionary", parent=self)
+        self.buttonValidateHealCsvDd.clicked.connect(self.validate_heal_csv_dd)
+        
+        # maybe switch Line edit to this: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QPlainTextEdit.html#more
+        #self.userMessageBox = QtWidgets.QLineEdit(parent=self)
+        self.userMessageBox = QtWidgets.QTextEdit(parent=self)
+        self.userMessageBox.setReadOnly(True)
+        
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.buttonValidateHealCsvDd)
+        layout.addWidget(self.userMessageBox)
+
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    
+    def validate_heal_csv_dd(self):
+        print("need to implement validation")

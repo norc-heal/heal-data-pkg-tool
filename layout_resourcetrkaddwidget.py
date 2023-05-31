37aa4170ff47509946b4ca1fe51580e829723dd2
Andrea Tentner <tentner-andrea@norc.org>
2023-05-31 15:04:23 -0500

    implemented a check on create/mod time of the resource tracker file and the resource file when resource annotation files are added to the resource tracker - still need to implement a check for exact duplicate entries

--- layout_resourcetrkaddwidget.py
+++ layout_resourcetrkaddwidget.py
@@ -31,6 +31,7 @@ from jsonschema import validate
 from schema_resource_tracker import schema_resource_tracker
 
 from healdata_utils.validators.jsonschema import validate_against_jsonschema
+import datetime
 
 
 class ResourceTrkAddWindow(QtWidgets.QMainWindow):
@@ -77,6 +78,7 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
         ifileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select the Input Resource Txt Data file",
                (QtCore.QDir.homePath()), "Text (*.txt)")
         
+        
         # load resource file and convert to python object
         path = ifileName
         data = json.loads(Path(path).read_text())
@@ -96,10 +98,50 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
             self.userMessageBox.setText(messageText)
             return
         
-        # if valid, convert json to pd
+        # if valid, continue:
+
+        # get resource id to add to resource.id property in resource tracker file
+        ifileNameStem = Path(ifileName).stem
+        resIdNumStr = ifileNameStem.rsplit('-',1)[1]
+        resource_id = "resource-" + resIdNumStr
+        print("resource-id: ", resource_id)
+
+        # get resource tracker resource file creation and last modification datetime
+        restrk_c_timestamp = os.path.getctime(ifileName)
+        restrk_c_datetime = datetime.datetime.fromtimestamp(restrk_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+        print("restrk_c_datetime: ", restrk_c_datetime)
+        
+        restrk_m_timestamp = os.path.getmtime(ifileName)
+        restrk_m_datetime = datetime.datetime.fromtimestamp(restrk_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+        print("restrk_m_datetime: ", restrk_m_datetime)
+
+        # get resource creation and last modification datetime
+        res_c_timestamp = os.path.getctime(data["path"])
+        res_c_datetime = datetime.datetime.fromtimestamp(res_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+        print("res_c_datetime: ", res_c_datetime)
+
+        res_m_timestamp = os.path.getmtime(data["path"])
+        res_m_datetime = datetime.datetime.fromtimestamp(res_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+        print("res_m_datetime: ", res_m_datetime)
+
+        add_to_df_dict = {#"resource.id":[resource_id],
+                          "resource.id.num": [int(resIdNumStr)],  
+                          "resource.create.date.time": [res_c_datetime],
+                          "resource.mod.date.time": [res_m_datetime],
+                          "resource.mod.time.stamp": [res_m_timestamp],
+                          "restrk.create.date.time": [restrk_c_datetime],
+                          "restrk.mod.date.time": [restrk_m_datetime],
+                          "restrk.mod.time.stamp": [restrk_m_timestamp]}
+
+        add_to_df = pd.DataFrame(add_to_df_dict)
+
+        # convert json to pd df
         df = pd.json_normalize(data)
         print(df)
+        df = pd.concat([df,add_to_df], axis = 1)
+        print(df)
 
+        
         # get data package directory path
         parentFolderPath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Your Data Package Directory - Your Resource Tracker File lives here!')
         
@@ -111,9 +153,14 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
             output_path=os.path.join(parentFolderPath,"heal-csv-resource-tracker.csv")
             all_df = pd.read_csv(output_path)
             all_df = pd.concat([all_df, df], axis=0) # this will be a row append with outer join on columns - will help accommodate any changes to fields/schema over time
+            all_df.sort_values(by = ["resource.id.num", "restrk.mod.time.stamp"], inplace=True)
+            # drop any exact duplicate rows
+            #all_df.drop_duplicates(inplace=True) # drop_duplicates does not work when df includes list vars
+            #all_df = all_df[~all_df.astype(str).duplicated]
             
             # before writing to file may want to check for duplicate resource IDs and if duplicate resource IDs, ensure that 
-            # user wants to overwrite the earlier instance of the resource ID in the resource tracker
+            # user wants to overwrite the earlier instance of the resource ID in the resource tracker - right now, dup entries 
+            # for a resource are all kept as long as not exact dup (i.e. at least one thing has changed)
 
             all_df.to_csv(output_path, mode='w', header=True, index=False)
             #df.to_csv(output_path, mode='a', header=not os.path.exists(output_path), index=False)
@@ -125,8 +172,18 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
             self.userMessageBox.setText(messageText)
             return
         
-        
-        
+
+
+if __name__ == "__main__":
+    
+    #app = QtWidgets.QApplication(sys.argv)
+
+    #app.exec_()
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = ResourceTrkAddWindow()
+    window.show()
+    sys.exit(app.exec_())   
       
 
          

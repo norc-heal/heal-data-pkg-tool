c7a56ca163884d0db67eddfedf0316e7c5047303
Andrea Tentner <tentner-andrea@norc.org>
2023-05-31 17:34:49 -0500

    added functionality to add multiple resource files to resource tracker at once - deduplication of exact dupes does not appear to be working though

--- layout_resourcetrkaddwidget.py
+++ layout_resourcetrkaddwidget.py
@@ -75,73 +75,99 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
     def add_resource(self):
 
         # get resource file path
-        ifileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select the Input Resource Txt Data file",
+        ifileName, _ = QtWidgets.QFileDialog.getOpenFileNames(self, "Select the Input Resource Txt Data file(s)",
                (QtCore.QDir.homePath()), "Text (*.txt)")
         
+        if ifileName:
+
+            collect_df = pd.DataFrame([])
+            validFiles = []
+            print(collect_df.shape)
+
+            for filename in ifileName:
+                print(filename)
+
+                # load resource file and convert to python object
+                #path = ifileName
+                path = filename
+                data = json.loads(Path(path).read_text())
+                print(data)
+
+                # validate experiment file json content against experiment tracker json schema
+                out = validate_against_jsonschema(data, schema_resource_tracker)
+                print(out["valid"])
+                print(out["errors"])
+
+                # print validation errors and exit if not valid
+                if out["valid"]:
+                    #messageText = "The following resource file is valid: " + ifileName
+                    messageText = "The following resource file is valid: " + filename
+                    self.userMessageBox.setText(messageText)
+                                        
+                else:
+                    #messageText = "The following resource file is NOT valid and will not be added to your Resource Tracker file: " + ifileName + "\n\n\n" + "Validation errors are as follows: " + out["errors"] + "\n\n\n" + "Exiting \"Add Resource\" function now."
+                    messageText = "The following resource file is NOT valid and will not be added to your Resource Tracker file: " + filename + "\n\n\n" + "Validation errors are as follows: " + out["errors"] + "\n\n\n" + "Exiting \"Add Resource\" function now."
+                    
+                    self.userMessageBox.setText(messageText)
+                    #return
+                    # switch from return to break so that if user selects more than one file, and one is not valid, can skip to next file and continue instead of returning fully out of the function
+                    break 
+
+                # if valid, continue:
+
+                # get resource id to add to resource.id property in resource tracker file
+                #ifileNameStem = Path(ifileName).stem
+                ifileNameStem = Path(filename).stem
+                resIdNumStr = ifileNameStem.rsplit('-',1)[1]
+                resource_id = "resource-" + resIdNumStr
+                print("resource-id: ", resource_id)
+                validFiles.append(ifileNameStem)
+                print("valid files:", validFiles)
+
+                # get resource tracker resource file creation and last modification datetime
+                #restrk_c_timestamp = os.path.getctime(ifileName)
+                restrk_c_timestamp = os.path.getctime(filename)
+                restrk_c_datetime = datetime.datetime.fromtimestamp(restrk_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+                print("restrk_c_datetime: ", restrk_c_datetime)
         
-        # load resource file and convert to python object
-        path = ifileName
-        data = json.loads(Path(path).read_text())
-        print(data)
+                #restrk_m_timestamp = os.path.getmtime(ifileName)
+                restrk_m_timestamp = os.path.getmtime(filename)
+                restrk_m_datetime = datetime.datetime.fromtimestamp(restrk_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+                print("restrk_m_datetime: ", restrk_m_datetime)
 
-        # validate experiment file json content against experiment tracker json schema
-        out = validate_against_jsonschema(data, schema_resource_tracker)
-        print(out["valid"])
-        print(out["errors"])
+                # get resource creation and last modification datetime
+                res_c_timestamp = os.path.getctime(data["path"])
+                res_c_datetime = datetime.datetime.fromtimestamp(res_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+                print("res_c_datetime: ", res_c_datetime)
 
-        # print validation errors and exit if not valid
-        if out["valid"]:
-            messageText = "The following resource file is valid: " + ifileName
-            self.userMessageBox.setText(messageText)
-        else:
-            messageText = "The following resource file is NOT valid and will not be added to your Resource Tracker file: " + ifileName + "\n\n\n" + "Validation errors are as follows: " + out["errors"] + "\n\n\n" + "Exiting \"Add Resource\" function now."
-            self.userMessageBox.setText(messageText)
+                res_m_timestamp = os.path.getmtime(data["path"])
+                res_m_datetime = datetime.datetime.fromtimestamp(res_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
+                print("res_m_datetime: ", res_m_datetime)
+
+                add_to_df_dict = {#"resource.id":[resource_id],
+                                "resource.id.num": [int(resIdNumStr)],  
+                                "resource.create.date.time": [res_c_datetime],
+                                "resource.mod.date.time": [res_m_datetime],
+                                "resource.mod.time.stamp": [res_m_timestamp],
+                                "restrk.create.date.time": [restrk_c_datetime],
+                                "restrk.mod.date.time": [restrk_m_datetime],
+                                "restrk.mod.time.stamp": [restrk_m_timestamp]}
+
+                add_to_df = pd.DataFrame(add_to_df_dict)
+
+                # convert json to pd df
+                df = pd.json_normalize(data)
+                print(df)
+                df = pd.concat([df,add_to_df], axis = 1)
+                print(df)
+
+                collect_df = pd.concat([collect_df,df], axis=0)
+                print("collect_df rows: ", collect_df.shape[0])
+
+        else: 
+            print("you have not selected any files; returning")
             return
-        
-        # if valid, continue:
 
-        # get resource id to add to resource.id property in resource tracker file
-        ifileNameStem = Path(ifileName).stem
-        resIdNumStr = ifileNameStem.rsplit('-',1)[1]
-        resource_id = "resource-" + resIdNumStr
-        print("resource-id: ", resource_id)
-
-        # get resource tracker resource file creation and last modification datetime
-        restrk_c_timestamp = os.path.getctime(ifileName)
-        restrk_c_datetime = datetime.datetime.fromtimestamp(restrk_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
-        print("restrk_c_datetime: ", restrk_c_datetime)
-        
-        restrk_m_timestamp = os.path.getmtime(ifileName)
-        restrk_m_datetime = datetime.datetime.fromtimestamp(restrk_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
-        print("restrk_m_datetime: ", restrk_m_datetime)
-
-        # get resource creation and last modification datetime
-        res_c_timestamp = os.path.getctime(data["path"])
-        res_c_datetime = datetime.datetime.fromtimestamp(res_c_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
-        print("res_c_datetime: ", res_c_datetime)
-
-        res_m_timestamp = os.path.getmtime(data["path"])
-        res_m_datetime = datetime.datetime.fromtimestamp(res_m_timestamp).strftime("%Y-%m-%d, %H:%M:%S")
-        print("res_m_datetime: ", res_m_datetime)
-
-        add_to_df_dict = {#"resource.id":[resource_id],
-                          "resource.id.num": [int(resIdNumStr)],  
-                          "resource.create.date.time": [res_c_datetime],
-                          "resource.mod.date.time": [res_m_datetime],
-                          "resource.mod.time.stamp": [res_m_timestamp],
-                          "restrk.create.date.time": [restrk_c_datetime],
-                          "restrk.mod.date.time": [restrk_m_datetime],
-                          "restrk.mod.time.stamp": [restrk_m_timestamp]}
-
-        add_to_df = pd.DataFrame(add_to_df_dict)
-
-        # convert json to pd df
-        df = pd.json_normalize(data)
-        print(df)
-        df = pd.concat([df,add_to_df], axis = 1)
-        print(df)
-
-        
         # get data package directory path
         parentFolderPath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Your Data Package Directory - Your Resource Tracker File lives here!')
         
@@ -152,14 +178,16 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
             
             output_path=os.path.join(parentFolderPath,"heal-csv-resource-tracker.csv")
             all_df = pd.read_csv(output_path)
-            all_df = pd.concat([all_df, df], axis=0) # this will be a row append with outer join on columns - will help accommodate any changes to fields/schema over time
+            #all_df = pd.concat([all_df, df], axis=0) # this will be a row append with outer join on columns - will help accommodate any changes to fields/schema over time
+            all_df = pd.concat([all_df, collect_df], axis=0) # this will be a row append with outer join on columns - will help accommodate any changes to fields/schema over time
+            
             all_df.sort_values(by = ["resource.id.num", "restrk.mod.time.stamp"], inplace=True)
             # drop any exact duplicate rows
             #all_df.drop_duplicates(inplace=True) # drop_duplicates does not work when df includes list vars
             # this current approach does not appear to be working at the moment
-            print("rows: ", all_df.shape[0])
+            print("all_df rows, with dupes: ", all_df.shape[0])
             all_df = all_df[-(all_df.astype('string').duplicated())]
-            print("rows: ", all_df.shape[0])
+            print("all_df rows, without dupes: ", all_df.shape[0])
             
             # before writing to file may want to check for duplicate resource IDs and if duplicate resource IDs, ensure that 
             # user wants to overwrite the earlier instance of the resource ID in the resource tracker - right now, dup entries 
@@ -168,7 +196,7 @@ class ResourceTrkAddWindow(QtWidgets.QMainWindow):
             all_df.to_csv(output_path, mode='w', header=True, index=False)
             #df.to_csv(output_path, mode='a', header=not os.path.exists(output_path), index=False)
 
-            messageText = messageText + "\n\n\n" + "The contents of the Resource file: " + "\n\n\n" + ifileName + "\n\n\n" + "were added as a resource to the Resource Tracker file: " + "\n\n\n" + output_path
+            messageText = messageText + "\n\n\n" + "The contents of the Resource file(s): " + "\n\n\n" + ', '.join(validFiles) + "\n\n\n" + "were added as a resource(s) to the Resource Tracker file: " + "\n\n\n" + output_path
             self.userMessageBox.setText(messageText)
         else:
             messageText = messageText + "\n\n\n" + "No Resource Tracker file exists at the designated directory. Are you sure this is a Data Package Directory? If you haven't yet created a Data Package Directory for your work, please head to the \"Data Package\" tab and use the \"Create new Data Package\" button to create your Data Package Directory. Your new Data Package Directory will contain your Resource Tracker file. You can then come back here and try adding your resource file again!" + "\n\n\n" + "Exiting \"Add Resource\" function now."

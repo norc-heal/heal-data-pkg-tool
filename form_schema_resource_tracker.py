49ae2c67c96e1b9e662f8e838ff3915d93aa523c
Andrea Tentner <tentner-andrea@norc.org>
2023-06-13 16:11:29 -0500

    moved some fields around to better indicate priority by order; added a priority property to each of the fields which can be used to add visual markers in the form indicating priority of the field and highlight the field only when other form values indicate it is appropriate to fill out the field (e.g. highlight the associated data dictionary fields when user indicates that resource is tabular data in the category field; deleted data from category field values and added tabular data and non-tabular data values

--- form_schema_resource_tracker.py
+++ form_schema_resource_tracker.py
@@ -23,112 +23,127 @@ form_schema_resource_tracker = {
             "title": "Resource ID",
             "description": "resource id; auto-assigned", 
             "type": "string",
-            "pattern": "^resource-+-*[0-9]*[1-9][0-9]*$"
+            "pattern": "^resource-+-*[0-9]*[1-9][0-9]*$",
+            "priority": "all, high"
+        },
+        "path": {
+            "title": "Resource File Path",
+            "description": "this will be auto-inferred as the full file path to resource",
+            "type": "string",
+            "format": "path",
+            "priority": "all, high"
+        },
+        "description": {
+            "title": "Resource Description",
+            "description": "description of resource",
+            "type": "string",
+            "priority": "all, high"
+        },
+        "category": {
+            "title" : "Resource Category",
+            "description": "options are multi-result (a file that includes more than one result in the form of a figure, text, etc.), single result, data, metadata, code",
+            "type": "string",
+            "enum": ["","multi-result","single result","tabular data","non-tabular data","metadata","code"],
+            "priority": "all, high"
         },
         "exp.belongs.to": {
             "title": "Experiment Belong To",
             "description": "if the file pertains specifically to one of the study experiments, list here; if the file pertains to more than one experiment, or to all experiments/the study as a whole, leave this blank; use experiment ID as assigned/formatted in your Experiment Tracker file here; prefix is 'exp-' followed by a number starting with 1 for the first experiment, and iterating by 1 for each successive experiment (i.e. exp-1, exp-2, etc.)",
             "type": "string",
-            "pattern": "^exp-+-*[0-9]*[1-9][0-9]*$"
+            "pattern": "^exp-+-*[0-9]*[1-9][0-9]*$",
+            "priority": "all, low"
         },
         #"name": {
         #    "title" : "Resource Name",
         #    "description": "this will be auto-inferred as the file name part of the file path",
         #    "type": "string"
         #},
-        "path": {
-            "title": "Resource File Path",
-            "description": "this will be auto-inferred as the full file path to resource",
-            "type": "string",
-            "format": "path"
-        },
         "title": {
             "title": "Resource Title",
             "description": "human-readable title/name of resource",
-            "type": "string"
-        },
-        "description": {
-            "title": "Resource Description",
-            "description": "description of resource",
-            "type": "string"
+            "type": "string",
+            "priority": "all, low"
         },
         "description.row": {
             "title": "Resource Row Description",
             "description": "for tabular data resource, row description; e.g. one row represents one subject at one timepoint",
-            "type": "string"
+            "type": "string",
+            "priority": "tabular data"
+        },
+        "description.file.name.convention": {
+            "title": "Resource File Name Convention",
+            "description": "for multi-file resource containing multiple files of the same type, provide the naming convention of files (e.g. subject-xx-protocol-xxx-day-xxxxxxxx)",
+            "type": "string",
+            "priority": "multiple like resource"
         },
         "description.file": {
             "title": "Resource File Description",
             "description": "for multi-file resource containing multiple files of the same type, component file description",
-            "type": "string"
-        },
-        "description.file.name.convention": {
-            "title": "Resource File Name Convention Description",
-            "description": "for multi-file resource containing multiple files of the same type, provide the naming convention of files (e.g. subject-xx-protocol-xxx-day-xxxxxxxx)",
-            "type": "string"
-        },
-        "category": {
-            "title" : "Resource Category",
-            "description": "options are multi-result (a file that includes more than one result in the form of a figure, text, etc.), single result, data, metadata, code",
             "type": "string",
-            "enum": ["","multi-result","single result","data","metadata","code"]
+            "priority": "multiple like resource"
         },
         "category.sub.metadata": {
             "title" : "Metadata Resource - Sub-Category",
             "description": "",
             "type": "string",
-            "enum": ["","data-dictionary","protocol","id-map","analysis plan","results-tracker","experiment-tracker"]
+            "enum": ["","data-dictionary","protocol","id-map","analysis plan","results-tracker","experiment-tracker"],
+            "priority": "metadata"
         },
         "category.sub.data": {
             "title" : "Data Resource - Sub-Category",
             "description": "",
             "type": "string",
-            "enum": ["","raw","processed-intermediate","processed-final"]
+            "enum": ["","raw","processed-intermediate","processed-final"],
+            "priority": "data"
         },
         "category.sub.results": {
             "title" : "Results Resource - Sub-Category",
             "description": "",
             "type": "string",
-            "enum": ["","figure","text","draft publication","publication"]
+            "enum": ["","figure","text","draft publication","publication"],
+            "priority": "results"
         },
         "assoc.file.dd": {
-            "title": "Data Resource - Associated Data Dictionary",
+            "title": "Associated Data Dictionary",
             "description": "generally relevant only for tabular data file resources; reference/file path to associated data dictionary file(s) - preferably in heal csv data dictionary format",
             #"type": "string"
             "type": "array",
             "items": {
                 "type": "string",
                 "format": "path"
-            }
+            },
+            "priority": "tabular data"
         },
         "assoc.file.protocol": {
-            "title": "Data Resource - Associated Protocol",
+            "title": "Associated Protocol",
             "description": "generally relevant only for data file resources; reference/file path to associated protocol file(s)",
             #"type": "string"
             "type": "array",
             "items": {
                 "type": "string",
                 "format": "path"
-            }
+            },
+            "priority": "data"
         },
-        "assoc.file.id.map": {
-            "title": "Data Resource - Associated ID Map",
-            "description": "generally relevant only for tabular data file resources; reference/file path to associated ID map file(s); e.g. if each row in a tabular data file represents a unique study subject at a unique study timepoint and the study subject is denoted by an ID in the data file, there may be an associated ID map that maps the subject ID to demographic variables relevant to each subject.",
-            #"type": "string"
-            "type": "array",
-            "items": {
-                "type": "string",
-                "format": "path"
-            }
-        },
+        #"assoc.file.id.map": {
+        #    "title": "Data Resource - Associated ID Map",
+        #    "description": "generally relevant only for tabular data file resources; reference/file path to associated ID map file(s); e.g. if each row in a tabular data file represents a unique study subject at a unique study timepoint and the study subject is denoted by an ID in the data file, there may be an associated ID map that maps the subject ID to demographic variables relevant to each subject.",
+        #    #"type": "string"
+        #    "type": "array",
+        #    "items": {
+        #        "type": "string",
+        #        "format": "path"
+        #    }
+        #},
         "assoc.file.depends.on": {
-            "title": "Source Files/Dependencies",
+            "title": "Other Associated Files/Dependencies",
             "description": "if the current resource file has dependencies/if other files are necessary to make this file (e.g. raw data file necessary to  make processed data file) list them here; only one layer deep; can be data, code, protocol (?); if already listed under assoc.file.dd, assoc.file.protocol, or assoc.file.id.map no need to repeat here. Alternatively, can use this field as a catch-all instead of using those other more specific assoc.file fields.",
             "type": "array",
             "items": {
                 "type": "string",
                 "format": "path"
-            }
+            },
+            "priority": "all, high"
         },
         "access": {
             "title" : "Access",
@@ -137,13 +152,15 @@ form_schema_resource_tracker = {
             "items":{
                 "type": "string",
                 "enum": ["","permanent-private","temporary-private","restricted-access","public"]
-            }
+            },
+            "priority": "all, high"
         },
         "access.date": {
             "title": "Access Date (YYYY/MM/DD or YYYY-MM-DD)",
             "description": "If the resource file is currently being held as temporary-private access level and will transition to either restricted-access or public access level at some point, please provide an anticipated date at which this transition will occur - Best guesses are appreciated, however you will NOT be held to this date and may update this date at any time.",
             "type": "string",
-            "pattern": "(((19|20)([2468][048]|[13579][26]|0[48])|2000)[/-]02[/-]29|((19|20)[0-9]{2}[/-](0[4678]|1[02])[/-](0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}[/-](0[1359]|11)[/-](0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}[/-]02[/-](0[1-9]|1[0-9]|2[0-8])))"
+            "pattern": "(((19|20)([2468][048]|[13579][26]|0[48])|2000)[/-]02[/-]29|((19|20)[0-9]{2}[/-](0[4678]|1[02])[/-](0[1-9]|[12][0-9]|30)|(19|20)[0-9]{2}[/-](0[1359]|11)[/-](0[1-9]|[12][0-9]|3[01])|(19|20)[0-9]{2}[/-]02[/-](0[1-9]|1[0-9]|2[0-8])))",
+            "priority": "temporary private"
         },
         #"format": {
         #    "title": "Format",
@@ -153,7 +170,8 @@ form_schema_resource_tracker = {
         "format.software": {
             "title": "Software used to produce/read the resource file",
             "description": "if the format is proprietary and requires specific software to open/interpret, provide the software name and version; e.g. Origin 11.0, CorelDraw 5.6",
-            "type": "string"
+            "type": "string",
+            "priority": "all, low"
         }
         #,
         #"format.open.type": {

0727ff2e7c5da6835ece3f52ba76db7e5c6ad52c
Andrea Tentner <tentner-andrea@norc.org>
2023-06-14 14:55:48 -0500

    added a context menu to the list widget that allows user to right click an item and get option to delete that item

--- layout_fileurladdwidget.py
+++ layout_fileurladdwidget.py
@@ -1,14 +1,54 @@
 # https://www.youtube.com/watch?v=KVEIW2htw0A
 
 import sys, os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QListWidget, QListWidgetItem, QPushButton, QMenu, QAction
+from PyQt5.QtCore import Qt, QUrl, QEvent
 
 class ListboxWidget(QListWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setAcceptDrops(True)
         #self.resize(600,600)
+        self.installEventFilter(self)
+
+    def eventFilter(self, source, event):
+        if event.type() == QEvent.ContextMenu and source is self:
+            print(event)
+            print(source)
+            self.menu = QMenu()
+            
+            self.rightClickDelete = QAction("Delete", self)
+            self.rightClickSelect = QAction("Select", self)
+            
+            self.menu.addAction(self.rightClickDelete)
+            #self.menu.addAction(self.rightClickSelect)
+            
+            if self.menu.exec_(event.globalPos()):
+                item = source.itemAt(event.pos())
+                print('i did it here!')
+                print(item.text())
+                self.takeItem(self.row(item)) 
+
+                #self.menu.triggered[QAction].connect(self.processtrigger)
+                #self.rightClickDelete.triggered.connect(lambda checked: right_click_delete())
+                
+            #self.menu.triggered[QAction].connect(self.processtrigger)
+            #self.rightClickDelete.triggered.connect(lambda checked: right_click_delete())
+                
+            return True
+
+            
+
+        return super().eventFilter(source, event)
+
+    def processtrigger(self,q):
+      print(q.text()+" is triggered")
+
+    def right_click_delete(self):
+        print("i did it!")
+        #print(bool)
+        #print(q.text()+" is triggered")
+
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasUrls:

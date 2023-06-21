36b69b52491674ab656e8c0e11334350577f80a5
Andrea Tentner <tentner-andrea@norc.org>
2023-06-21 09:22:35 -0500

    updated get multi like file descriptions fx to output messages as well as description list so that can print error to user status box

--- dsc_pkg_utils.py
+++ dsc_pkg_utils.py
@@ -254,6 +254,9 @@ def validateJson(jsonData,jsonSchema):
     return True
     
 def get_multi_like_file_descriptions(nameConvention,fileStemList):
+    
+    allFilesDescribeList = [] 
+    messagesOut = []
 
     nameConventionExplanatoryList = re.findall('{(.+?)}', nameConvention) # list of items enclosed in curly braces
     print(nameConventionExplanatoryList)
@@ -275,7 +278,8 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                 messageText = "Naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon."
                 #errorFormat = '<span style="color:red;">{}</span>'
                 #self.userMessageBox.append(errorFormat.format(messageText))
-                return messageText
+                messagesOut.append(messageText)
+                return allFilesDescribeList, messagesOut
 
         if not nameOnlyOneExplanatory:
             noDelim = 0
@@ -293,6 +297,7 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                             noDelim += 1
                             print(l," and ",afterVal," naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon.")
                             messageText = l," and ",afterVal," naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon."
+                            messagesOut.append(messageText)
                             #errorFormat = '<span style="color:red;">{}</span>'
                             #self.userMessageBox.append(errorFormat.format(messageText))
                     if beforeVal:
@@ -300,20 +305,23 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                             noDelim += 1
                             print(beforeVal," and ",l," naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon.")
                             messageText = beforeVal," and ",l," naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon."
+                            messagesOut.append(messageText)
                             #errorFormat = '<span style="color:red;">{}</span>'
                             #self.userMessageBox.append(errorFormat.format(messageText))
 
             if noDelim > 0:
                 print("Naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon.")
                 messageText = "Naming convention explanatory values are not separated by a delimiter in your file name convention. Please add delimiters between file naming convention explanatory values in your filenames. Snake case as a delimiter is not currently supported but will be soon."
+                messagesOut.append(messageText)
                 #errorFormat = '<span style="color:red;">{}</span>'
                 #self.userMessageBox.append(errorFormat.format(messageText))
-                return
+                return allFilesDescribeList, messagesOut
 
 
         allFilesDescribeList = [] 
 
         for i in fileStemList:
+            print(i)
         
             oneFileDescribeList = []  
 
@@ -322,6 +330,7 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
             else: 
             
                 for idx, l in enumerate(nameConventionAllList):
+                    print("idx: ",idx,", l: ",l)
                     beforeVal = None
                     beforeValSplit = None
                     afterVal = None
@@ -329,8 +338,10 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                     if l in nameConventionExplanatoryList:
                         if idx != len(nameConventionAllList) - 1: # if not last element, get element after current element  
                             afterVal = nameConventionAllList[idx + 1]
+                            print("afterVal: ",afterVal)
                         if idx != 0: # if not first element, get element first current element  
                             beforeVal = nameConventionAllList[idx - 1]
+                            print("beforeVal: ",beforeVal)
             
                         if afterVal:
                             if afterVal in i:
@@ -339,6 +350,7 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                             else:
                                 #print("the file named ", i, " does not conform to the specified naming convention. It does not contain the string ",afterVal," as specified by the applied naming convention.")
                                 messageText = "the file named ", i, " does not conform to the specified naming convention. It does not contain the string ",afterVal," as specified by the applied naming convention."
+                                messagesOut.append(messageText)
                                 #errorFormat = '<span style="color:red;">{}</span>'
                                 #self.userMessageBox.append(errorFormat.format(messageText))
                                 continue
@@ -350,6 +362,7 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                             else:
                                 #print("the file named ", i, " does not conform to the specified naming convention. It does not contain the string ",beforeVal," as specified by the applied naming convention.")
                                 messageText = "the file named ", i, " does not conform to the specified naming convention. It does not contain the string ",beforeVal," as specified by the applied naming convention."
+                                messagesOut.append(messageText)
                                 #errorFormat = '<span style="color:red;">{}</span>'
                                 #self.userMessageBox.append(errorFormat.format(messageText))
                                 continue
@@ -357,6 +370,7 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
                         if ((not afterValSplit) and (not beforeValSplit)):
                             #print("the file named ", i, " does not conform to the specified naming convention. It does not contain string(s) specified by the applied naming convention. Exiting check of this file.")
                             messageText = "the file named ", i, " does not conform to the specified naming convention. It does not contain string(s) specified by the applied naming convention. Exiting check of this file."
+                            messagesOut.append(messageText)
                             #errorFormat = '<span style="color:red;">{}</span>'
                             #self.userMessageBox.append(errorFormat.format(messageText))
                             break
@@ -380,6 +394,6 @@ def get_multi_like_file_descriptions(nameConvention,fileStemList):
             allFilesDescribeList.append(oneFileDescribe) 
             print(allFilesDescribeList) 
     
-    return allFilesDescribeList  
+    return allFilesDescribeList, messagesOut  
 
     

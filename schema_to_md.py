f1d80cfdda1cc5a9d28236acd7c9e19a54b6533d
Andrea Tentner <tentner-andrea@norc.org>
2023-08-23 13:48:13 -0500

    script to convert schemas to md; created md version for current status schemas

---
+++ schema_to_md.py
@@ -0,0 +1,40 @@
+import json
+import jsonschema2md
+import os
+import sys
+
+#################################################
+
+from schema_experiment_tracker import schema_experiment_tracker
+from schema_resource_tracker import schema_resource_tracker
+from schema_results_tracker import schema_results_tracker
+
+md_experiment_tracker = ".\md_experiment_tracker.md"
+md_resource_tracker = ".\md_resource_tracker.md"
+md_results_tracker = ".\md_results_tracker.md"
+
+#################################################
+
+input_schema = schema_results_tracker
+output_md = md_results_tracker
+
+#################################################
+
+parser = jsonschema2md.Parser(
+    examples_as_yaml=False,
+    show_examples="all",
+)
+#with open("./examples/food.json", "r") as json_file:
+#    md_lines = parser.parse_schema(json.load(json_file))
+#print(''.join(md_lines))
+
+md_lines = parser.parse_schema(input_schema)
+print(''.join(md_lines))
+
+original_stdout = sys.stdout # save ref to original stdout of print
+
+with open(output_md,"w") as f:
+    sys.stdout = f # change stdout to output md file we created
+    print(''.join(md_lines))
+    sys.stdout = original_stdout # reset stdout to original value
+

cf6d58d6d8b571b0845448855148f51d23c852bd
Andrea Tentner <tentner-andrea@norc.org>
2023-09-06 18:18:31 -0500

    switched view/edit csv to view only to discourage editing the tracker directly

--- layout_resultstrktabswidget.py
+++ layout_resultstrktabswidget.py
@@ -15,7 +15,8 @@ from PyQt5.QtWidgets import (
 from layout_resultstrkaddwidget import ResultsTrkAddWindow
 from layout_resultstrkcreatewidget import ResultsTrkCreateWindow
 from layout_infotextwidget import InfoTextWindow
-from layout_csvpushtoloadwidget import CSVPushToLoadWindow
+from layout_csvviewpushtoloadwidget import CSVViewPushToLoadWindow
+#from layout_csvpushtoloadwidget import CSVPushToLoadWindow
 
 class ResultsTrkTabsWindow(QWidget):
     def __init__(self):
@@ -34,29 +35,29 @@ class ResultsTrkTabsWindow(QWidget):
         tabs.addTab(InfoTextWindow(self.infoText), "Info")
         tabs.addTab(ResultsTrkCreateWindow(), "Create Result Tracker")
         tabs.addTab(ResultsTrkAddWindow(), "Add Results")
-        tabs.addTab(CSVPushToLoadWindow(), "View/Edit Tracker")
+        tabs.addTab(CSVViewPushToLoadWindow(), "View Tracker")
         
         
                 
         layout.addWidget(tabs)
 
-    def generalTabUI(self):
-        """Create the General page UI."""
-        generalTab = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(QCheckBox("General Option 1"))
-        layout.addWidget(QCheckBox("General Option 2"))
-        generalTab.setLayout(layout)
-        return generalTab
+    # def generalTabUI(self):
+    #     """Create the General page UI."""
+    #     generalTab = QWidget()
+    #     layout = QVBoxLayout()
+    #     layout.addWidget(QCheckBox("General Option 1"))
+    #     layout.addWidget(QCheckBox("General Option 2"))
+    #     generalTab.setLayout(layout)
+    #     return generalTab
 
-    def networkTabUI(self):
-        """Create the Network page UI."""
-        networkTab = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(QCheckBox("Network Option 1"))
-        layout.addWidget(QCheckBox("Network Option 2"))
-        networkTab.setLayout(layout)
-        return networkTab
+    # def networkTabUI(self):
+    #     """Create the Network page UI."""
+    #     networkTab = QWidget()
+    #     layout = QVBoxLayout()
+    #     layout.addWidget(QCheckBox("Network Option 1"))
+    #     layout.addWidget(QCheckBox("Network Option 2"))
+    #     networkTab.setLayout(layout)
+    #     return networkTab
 
 
 if __name__ == "__main__":

1994b8787a653d24e4ee8d43926519e0563bd677
Andrea Tentner <tentner-andrea@norc.org>
2023-09-06 18:07:46 -0500

    made a version of the widget that has a button to load the csv viewer window that only loads the view only version; last commit message was for the wrong commit - that commit was just adding a main call so that can run the widget with a button to load csv viewer by itself

---
+++ layout_csvviewpushtoloadwidget.py
@@ -0,0 +1,72 @@
+import csv, codecs # base python, no pip install needed
+import os # base python, no pip install needed
+ 
+from PyQt5 import QtCore, QtGui, QtWidgets, QtPrintSupport 
+from PyQt5.QtGui import QImage, QPainter
+from PyQt5.QtCore import QFile, Qt
+
+import sys # base python, no pip install needed
+
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.uic import loadUi
+
+from pathlib import Path # base python, no pip install needed
+
+from healdata_utils.cli import convert_to_vlmd
+
+#from frictionless import plugins # frictionless already installed as a healdata_utils dependency, no pip install needed
+#from frictionless.plugins import remote
+#from frictionless import describe
+
+import pandas as pd # pandas already installed as a healdata_utils dependency, no pip install needed
+import json # base python, no pip install needed
+import pipe
+
+import dsc_pkg_utils # local module, no pip install needed
+#from layout_csveditwidget import CSVEditWindow
+from layout_csvviewwidget import CSVViewWindow
+
+class CSVViewPushToLoadWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.w = None  # No external window yet.
+        
+        widget = QtWidgets.QWidget()
+        
+        self.buttonViewCsv = QtWidgets.QPushButton(text="View CSV", parent=self)
+        self.buttonViewCsv.clicked.connect(self.view_csv)
+        
+
+        # maybe switch Line edit to this: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QPlainTextEdit.html#more
+        #self.userMessageBox = QtWidgets.QLineEdit(parent=self)
+        self.userMessageBox = QtWidgets.QTextEdit(parent=self)
+        self.userMessageBox.setReadOnly(True)
+        
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.buttonViewCsv)
+        layout.addWidget(self.userMessageBox)
+
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    
+    def view_csv(self,checked):
+        if self.w is None:
+            self.w = CSVViewWindow('')
+            self.w.show()
+
+        else:
+            self.w.close()  # Close window.
+            self.w = None  # Discard reference.
+
+if __name__ == "__main__":
+    
+    #app = QtWidgets.QApplication(sys.argv)
+
+    #app.exec_()
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = CSVViewPushToLoadWindow()
+    window.show()
+    sys.exit(app.exec_())

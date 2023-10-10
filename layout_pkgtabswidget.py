7aed6b2528a8cfe1bc1983534ac4739fe734795b
Andrea Tentner <tentner-andrea@norc.org>
2023-10-10 17:53:18 -0500

    swapped the infotext widget for infotextbrowser widget to allow clickable hyperlink

--- layout_pkgtabswidget.py
+++ layout_pkgtabswidget.py
@@ -10,9 +10,12 @@ from PyQt5.QtWidgets import (
     QWidget,
 )
 
+
+
 from layout_pkgcreatewidget import PkgCreateWindow
-from layout_infotextwidget import InfoTextWindow
+#from layout_infotextwidget import InfoTextWindow
 from layout_csvpushtoloadwidget import CSVPushToLoadWindow
+from layout_infotextbrowsewidget import InfoTextBrowserWindow
 
 
         
@@ -26,36 +29,36 @@ class PkgTabsWindow(QWidget):
         # Create a top-level layout
         layout = QVBoxLayout()
         self.setLayout(layout)
-        self.infoText = "Start your data packaging and submission journey by creating a DSC Data Package directory! By creating your DSC Data Package directory and following the recommendations and process flow for data packaging set forth in the Data Packaging Recommendations and Process Flow document, you'll memorialize what each data and non-data/supporting file in your study data package is/represents, how these files related to each other, and how to drill down into these componenet data and non-data/supporting files to recreate specific published results. This will facilitate continuity and passed-down knowledge within study groups, and discovery, sharing, and re-use of the data and knowledge produced by the study outside of the original study group."
+        self.infoText = "Start your data packaging and submission journey by creating a DSC Data Package directory!" + "<br><br>" + "By creating your DSC Data Package directory and following the recommendations and process flow for data packaging set forth in the <a href=\"https://norc-heal.github.io/heal-data-pkg-guide/\"> HEAL Data Packaging </a> guidance website, you'll memorialize what each data and non-data/supporting file in your study data package is/represents, how these files related to each other, and how to drill down into these component data and non-data/supporting files to recreate specific published results. This will facilitate continuity and passed-down knowledge within study groups, and discovery, sharing, and re-use of the data and knowledge produced by the study outside of the original study group." + "<br><br>" + "1. <b>Start your Data Package:</b> <u>The first time you use this tool</u>, navigate to the \"Create or Continue Data Package\" tab to start your data package by creating a new data package directory in your study folder. This will automatically set your new data package directory as your working data package directory." + "<br><br>" + "2. <b>Continue your Data Package:</b> <u>Anytime you return to this tool to work on your data package</u>, navigate to the \"Create or Continue Data Package\" tab to continue your data package work by selecting your existing data package directory folder. This will set your existing data package directory as your working data package directory folder."
         
         
         # Create the tab widget 
         tabs = QTabWidget()
-        tabs.addTab(InfoTextWindow(infoText=self.infoText), "Info")
-        tabs.addTab(PkgCreateWindow(), "Create")
+        tabs.addTab(InfoTextBrowserWindow(infoText=self.infoText), "Info")
+        tabs.addTab(PkgCreateWindow(), "Create or Continue Data Package")
         #tabs.addTab(CSVPushToLoadWindow(), "View/Edit")
         
         layout.addWidget(tabs)
 
         
 
-    def generalTabUI(self):
-        """Create the General page UI."""
-        generalTab = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(QCheckBox("General Option 1"))
-        layout.addWidget(QCheckBox("General Option 2"))
-        generalTab.setLayout(layout)
-        return generalTab
+    # def generalTabUI(self):
+    #     """Create the General page UI."""
+    #     generalTab = QWidget()
+    #     layout = QVBoxLayout()
+    #     layout.addWidget(QCheckBox("General Option 1"))
+    #     layout.addWidget(QCheckBox("General Option 2"))
+    #     generalTab.setLayout(layout)
+    #     return generalTab
 
-    def networkTabUI(self):
-        """Create the Network page UI."""
-        networkTab = QWidget()
-        layout = QVBoxLayout()
-        layout.addWidget(QCheckBox("Network Option 1"))
-        layout.addWidget(QCheckBox("Network Option 2"))
-        networkTab.setLayout(layout)
-        return networkTab
+    # def networkTabUI(self):
+    #     """Create the Network page UI."""
+    #     networkTab = QWidget()
+    #     layout = QVBoxLayout()
+    #     layout.addWidget(QCheckBox("Network Option 1"))
+    #     layout.addWidget(QCheckBox("Network Option 2"))
+    #     networkTab.setLayout(layout)
+    #     return networkTab
 
 
 if __name__ == "__main__":

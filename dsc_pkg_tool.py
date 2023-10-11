81997c84c41bf47897119a03d6c74ad1a4812b3a
Andrea Tentner <tentner-andrea@norc.org>
2023-10-11 12:28:28 -0500

    added a read only text edit widget above the tabs widget on the tool main window that displays the working data package directory when one has been set; before one has been set it displays default text prompting user to set working data package dir and how to do so

--- dsc_pkg_tool.py
+++ dsc_pkg_tool.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import QFile, Qt
 
 import sys # base python, no pip install needed
 
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QVBoxLayout, QTextEdit
 from PyQt5.uic import loadUi
 
 from pathlib import Path # base python, no pip install needed
@@ -48,6 +48,8 @@ from layout_resultstrktabswidget import ResultsTrkTabsWindow
 #from layout_vlmdcreatewidget import VLMDCreateWindow
 from layout_csveditwidget import CSVEditWindow
 
+from layout_infotextbrowsewidget import InfoTextBrowserWindow
+
 # this will prevent windows from setting the app icon to python automatically based on .py suffix
 try:
     from ctypes import windll # only exists on windows, base python, no pip install needed
@@ -65,11 +67,25 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("DSC Data Packaging Tool - alpha")
 
+        self.main_widget = QtWidgets.QWidget(self)
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+        
+        # Create a top-level layout
+        self.layout = QVBoxLayout(self.main_widget)
+        #self.setLayout(self.layout)
+        
+        self.workingDataPkgDirDisplayDefaultText = "Set a working data package directory! <br><br> Navigate to the \"Create or Continue Data Package\" sub-tab of the \"Data Package\" tab to set a new or existing data package directory as your working data package directory"
+        self.workingDataPkgDirLabel = QLabel("Working Data Package Directory:", self)
+        self.workingDataPkgDirDisplay = QtWidgets.QTextEdit(parent=self)
+        self.workingDataPkgDirDisplay.setReadOnly(True)
+        self.workingDataPkgDirDisplay.setText(self.workingDataPkgDirDisplayDefaultText)
+        
         tabs = QTabWidget()
         tabs.setTabPosition(QTabWidget.West)
         tabs.setMovable(True)
 
-        tabs.addTab(PkgTabsWindow(), "Data Package")
+        tabs.addTab(PkgTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Data Package")
         tabs.addTab(ExpTrkTabsWindow(), "Experiment Tracker")
         tabs.addTab(ResourceTrkTabsWindow(), "Resource Tracker")
         tabs.addTab(VLMDTabsWindow(), "Data Dictionary")
@@ -77,9 +93,11 @@ class MainWindow(QMainWindow):
         
         #for n, color in enumerate(["red", "green", "blue", "yellow"]):
         #    tabs.addTab(Color(color), color)
-
+        self.layout.addWidget(self.workingDataPkgDirLabel)
+        self.layout.addWidget(self.workingDataPkgDirDisplay)
+        self.layout.addWidget(tabs)
         
-        self.setCentralWidget(tabs)
+        #self.setCentralWidget(self.layout)
 
 
 #app = QApplication(sys.argv)

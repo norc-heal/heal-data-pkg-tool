4b9eddaaa8dec43636fb41ee7cc69e9019290e8e
Andrea Tentner <tentner-andrea@norc.org>
2023-10-18 18:24:50 -0500

    schema_experiment_tracker.py

--- schema_experiment_tracker.py
+++ schema_experiment_tracker.py
@@ -5,10 +5,17 @@ schema_experiment_tracker = {
     "properties": {
         "experimentId": {
             "title": "Experiment ID",
-            "description": "id assigned to each experiment relevant to the data package; prefix is 'exp-' followed by a number starting with 1 for the first experiment, and iterating by 1 for each successive experiment (i.e. exp-1, exp-2, etc.)",
+            "description": "ID assigned to each experiment relevant to the data package; prefix is 'exp-' followed by a number starting with 1 for the first experiment, and iterating by 1 for each successive experiment (i.e. exp-1, exp-2, etc.)",
             "type": "string",
             "pattern": "^exp-+-*[0-9]*[1-9][0-9]*$",
             "priority": "all, high, auto"
+        },
+        "experimentName": {
+            "title": "Experiment Name",
+            "description": "If you may want to link specific study files or study results to a specific single study experiment or activity, it's a good idea to assign a human-recognizable \"experiment name\" to your study experiment or activity here. If you are using the DSC Data Packaging Tool, you will be able to select from a pick list of experiment names to make this link, which will be easier than linking by experiment ID. The Experiment Name should be short and descriptive, and must be unique. It is meant to be an alternative ID assigned to each experiment relevant to the data package that is human readable and recognizable as related to a specific study experiment or activity. In order to make this both human and machine readable, 1) name must start with a lower case letter, 2) name must end with a lower case letter or number, 3) only characters allowed are lower case letters, numbers, and hyphens (use hyphen instead of space), 4) name has a max length of 50, 5) name must be unique (append an iterable number as a suffix to make unique if necessary; e.g. \"-1\", \"-2\")",
+            "type": "string",
+            "pattern": "^(?=.{3,50}$)[a-z]+(-*)([a-z0-9]+)(-[a-z,0-9]+)*$",
+            "priority": "all, high"
         },
         "experimentType": {
             "title" : "Experiment Type",

7b7c102eae55ffb7c241c1fd9dc50a46f7ee004a
Andrea Tentner <tentner-andrea@norc.org>
2023-10-25 17:45:33 -0500

    change tab order - shift data dictionary tab to last tab

--- dsc_pkg_tool.py
+++ dsc_pkg_tool.py
@@ -88,8 +88,9 @@ class MainWindow(QMainWindow):
         tabs.addTab(PkgTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Data Package")
         tabs.addTab(ExpTrkTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Experiment Tracker")
         tabs.addTab(ResourceTrkTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Resource Tracker")
-        tabs.addTab(VLMDTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Data Dictionary")
         tabs.addTab(ResultsTrkTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Results Tracker")
+        tabs.addTab(VLMDTabsWindow(workingDataPkgDirDisplay=self.workingDataPkgDirDisplay), "Data Dictionary")
+        
         
         #for n, color in enumerate(["red", "green", "blue", "yellow"]):
         #    tabs.addTab(Color(color), color)

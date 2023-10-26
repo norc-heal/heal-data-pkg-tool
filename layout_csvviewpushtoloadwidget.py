a132b9ace2ec8e32b7b0f719fb1c32a413a13c22
Andrea Tentner <tentner-andrea@norc.org>
2023-10-26 12:41:26 -0500

    add check on if file exists and is closed

--- layout_csvviewpushtoloadwidget.py
+++ layout_csvviewpushtoloadwidget.py
@@ -37,8 +37,12 @@ class CSVViewPushToLoadWindow(QtWidgets.QMainWindow):
         self.fileTypeTitle = fileTypeTitle
         
         widget = QtWidgets.QWidget()
+
+        buttonText = "View CSV"
+        if self.fileTypeTitle:
+            buttonText = "View " + self.fileTypeTitle 
         
-        self.buttonViewCsv = QtWidgets.QPushButton(text="View CSV", parent=self)
+        self.buttonViewCsv = QtWidgets.QPushButton(text=buttonText, parent=self)
         self.buttonViewCsv.clicked.connect(self.view_csv)
         
 
@@ -63,8 +67,52 @@ class CSVViewPushToLoadWindow(QtWidgets.QMainWindow):
 
         if self.fileBaseName:
             self.fileName = os.path.join(self.workingDataPkgDir,self.fileBaseName)
+
+            # check that file exists in working data pkg dir, if not, return
+            if not os.path.exists(self.fileName):
+                messageText = "<br>There is no " + self.fileTypeTitle + " file in your working Data Package Directory; Your working Data Package Directory must contain a " + self.fileTypeTitle + " file to proceed - The file should be named: " + self.fileBaseName + ". If you need to change your working Data Package Directory or create a new one, head to the \"Data Package\" tab >> \"Create or Continue Data Package\" sub-tab to set a new working Data Package Directory or create a new one. <br><br>"
+                saveFormat = '<span style="color:red;">{}</span>'
+                self.userMessageBox.append(saveFormat.format(messageText))
+                return
+            
+            # check that file is closed (user doesn't have it open in excel for example)
+            try: 
+                with open(self.fileName,'r+') as f:
+                    print("file is closed, proceed!!")
+            except PermissionError:
+                    messageText = "<br>The " + self.fileTypeTitle + " file in your working Data Package Directory is open in another application, and must be closed to proceed; Check if the " + self.fileTypeTitle + " file is open in Excel or similar application, close the file, and try again. <br><br>"
+                    saveFormat = '<span style="color:red;">{}</span>'
+                    self.userMessageBox.append(saveFormat.format(messageText))
+                    return
+
+
         else:
             self.fileName = self.workingDataPkgDir
+
+            # check that all at least one of the desired file types exist to view and that all files of that type are closed (user doesn't have it open in excel for example)
+            trackersList = [filename for filename in os.listdir(self.workingDataPkgDir) if filename.startswith(self.fileStartsWith)]
+            print("trackersList: ", trackersList)
+            
+            if trackersList:
+                for tracker in trackersList:
+
+                    try: 
+                        with open(os.path.join(self.workingDataPkgDir,tracker),'r+') as f:
+                            print("file is closed, proceed!!")
+                    except PermissionError:
+                            messageText = "<br>At least one " + self.fileTypeTitle + " file that already exists in your working Data Package Directory is open in another application, and must be closed to proceed; Check if any " + self.fileTypeTitle + " files are open in Excel or similar application, close the file(s), and try again. <br><br>"
+                            saveFormat = '<span style="color:red;">{}</span>'
+                            self.userMessageBox.append(saveFormat.format(messageText))
+                            return
+            else: 
+                messageText = "<br>There are no " + self.fileTypeTitle + " files in your working Data Package Directory to view. There must be at least one " + self.fileTypeTitle + " file in your working Data Package Directory in order to proceed to view a file of this type. Files of this type should have a file name that starts with: " + self.fileStartsWith + ".<br><br>"
+                saveFormat = '<span style="color:red;">{}</span>'
+                self.userMessageBox.append(saveFormat.format(messageText))
+                return
+
+
+
+
 
         if self.w is None:
             self.w = CSVViewWindow(fileName=self.fileName,fileStartsWith=self.fileStartsWith,fileTypeTitle=self.fileTypeTitle)

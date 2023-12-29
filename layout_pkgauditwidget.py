533bd8826cee740b31ee6cf6ee2f009ba9d0f9a8
Andrea Tentner <tentner-andrea@norc.org>
2023-12-29 15:19:09 -0600

    start fleshing out the update function

--- layout_pkgauditwidget.py
+++ layout_pkgauditwidget.py
@@ -64,4 +64,72 @@ class PkgAuditWindow(QtWidgets.QMainWindow):
         
     def update_pkg_versions(self):
         print("update package versions")
+
+        # check if user has set a working data package dir - if not exit gracefully with informative message
+        if not dsc_pkg_utils.getWorkingDataPkgDir(self=self):
+            return
+
+        # check if update is necessary
+        checkVersions = version_check.version_check(self.workingDataPkgDir)
+        allUpToDate = checkVersions[0]
+        message = checkVersions[1]
+        collectDf = checkVersions[2]
+      
+        
+        if not allUpToDate:
+            # create a copy of working data pkg dir in which to do the update - 
+            # at the end can clean up but don't want the possibility that the update fails and the original is corrupted in the process
+            # if an update in progress folder already exists exit with informative message - this may indicate that the user had a previously failed update since a successful update would lead to clean up of this folder
+            if not dsc_pkg_utils.copyDataPkgDirToUpdate(self.workingDataPkgDir):
+                messageText = "<br>An update of your working data package directory may already be in progress. Check for a folder in the same parent directory as your working data package directory that starts with \"dsc-pkg\" and ends with \"update-in-progress\". If this folder exists, an update may have been initiated but not completed. If you didn't purposely create or keep this folder, please delete this folder and then come back here and try to update again. <br>"
+                saveFormat = '<span style="color:red;">{}</span>'
+                self.userMessageBox.append(saveFormat.format(messageText))
+                return
+            else:
+                messageText = "<br>An \"update-in-progress\" version of your working data package directory has been successfully created - This copy will be used to perform the updates and will be cleaned up at the end of a successful update - You shouls see a new folder in the same parent directory as your working data package directory that starts with \"dsc-pkg\" and ends with \"update-in-progress\".<br>"
+                saveFormat = '<span style="color:green;">{}</span>'
+                self.userMessageBox.append(saveFormat.format(messageText))
+
+
+            if "tracker" in collectDf["fileType"].values:
+                trackerDf = collectDf[collectDf["fileType"] == "tracker"]
+                
+                messageText = "<br>The following csv trackers were detected:<br>" + "<br>".join(trackerDf["file"].tolist())
+                self.userMessageBox.append(messageText)
+                
+                if "No" in trackerDf["upToDate"].values:
+                    trackerDfNeedsUpdate = trackerDf[trackerDf["upToDate"] == "No"]
+
+                    messageText = "<br>The following csv trackers need to be updated:<br>" + "<br>".join(trackerDfNeedsUpdate["file"].tolist())
+                    self.userMessageBox.append(messageText)
+                    
+                    if "Yes" in trackerDfNeedsUpdate["canBeUpdated"].values:
+                        trackerDfCanBeUpdated = trackerDfNeedsUpdate[trackerDfNeedsUpdate["canBeUpdated"] == "Yes"]
+
+                        messageText = "<br>The following csv trackers need to be updated AND can be updated:<br>" + "<br>".join(trackerDfCanBeUpdated["file"].tolist())
+                        self.userMessageBox.append(messageText)
+                    else:
+                        messageText = "<br>None of the csv trackers that need to be updated can be updated. This is likely because schema version mapping files for these trackers are not up to date."
+                        self.userMessageBox.append(messageText)
+
+
+                else:
+                    messageText = "<br>All csv trackers are up to date - json txt file updates coming soon<br>"
+                    saveFormat = '<span style="color:orange;">{}</span>'
+                    self.userMessageBox.append(saveFormat.format(messageText))
+                    return
+            else:
+                messageText = "<br>No csv trackers were detected - json txt file updates coming soon<br>"
+                saveFormat = '<span style="color:orange;">{}</span>'
+                self.userMessageBox.append(saveFormat.format(messageText))
+                return
+
+
+        else:
+
+            messageText = "<br>All dsc files are up to date - no updates needed!<br>"
+            saveFormat = '<span style="color:green;">{}</span>'
+            self.userMessageBox.append(saveFormat.format(messageText))
+            return
+
     

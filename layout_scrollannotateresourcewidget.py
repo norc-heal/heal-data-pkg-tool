6b812a859a20947a075ad29f935fe21a4c28a0cd
Andrea Tentner <tentner-andrea@norc.org>
2024-01-31 17:02:05 -0600

    in load file fx, check if the file selected is part of multi like file series; if in add based on mode return with message that you can't add based on using a file that is part of a multi like file resource; if in edit mode check if the file selected is the first in the series; if not then return with message that to edit a file that is part of a multi like file resource you have to select the file that is the first in the series

--- layout_scrollannotateresourcewidget.py
+++ layout_scrollannotateresourcewidget.py
@@ -1821,10 +1821,23 @@ class ScrollAnnotateResourceWindow(QtWidgets.QMainWindow):
 
             if self.mode == "add-based-on":
                 based_on_annotation_id = data["resourceId"]
+                
+                if data["associatedFileMultiLikeFiles"]: 
+                    messageText = "<br>You selected a resource txt file that is part of a multi-like file resource. It is currently not possible to add a new resource based upon an existing resource txt file that is part of a multi-like file resource. Instead, start a new resource from scratch using the \"Add a new resource\" push-button or start a new resource based on an existing resource using the \"Add a new resource based on an existing resource\" push-button, then select a resource txt file that is NOT part of a multi-like file resource upon which to base the new resource.<br><br> To proceed, close this form and return to the main DSC Data Packaging Tool window."
+                    saveFormat = '<span style="color:red;">{}</span>'
+                    self.userMessageBox.append(saveFormat.format(messageText))
+                    return
 
             if self.mode == "edit": 
                 # self.saveFilePath = ifileName # is this necessary?
                 # print("setting saveFilePath to path of chosen file")
+
+                if data["associatedFileMultiLikeFiles"]:
+                    if data["path"] != data["associatedFileMultiLikeFiles"][0]: 
+                        messageText = "<br>You selected a resource txt file that is part of a multi-like file resource, but you did NOT select the resource txt file that is the first in the series for this multi-like file resource. In order to edit a resource that is part of a multi-like file resource you MUST select the first resource txt file in the series for the multi-like file resource. You can head to the \"View Tracker\" tab to view your Resource Tracker, identify the resource ID for the first resource txt file in the series for this multi-like file resource, then try again.<br><br> To proceed, close this form and return to the main DSC Data Packaging Tool window."
+                        saveFormat = '<span style="color:red;">{}</span>'
+                        self.userMessageBox.append(saveFormat.format(messageText))
+                        return
                 
                 self.resource_id = data["resourceId"]
                 self.resIdNum = int(self.resource_id.split("-")[1])

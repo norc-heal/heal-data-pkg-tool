d279072c77e21795bd5e4ccc61d71c9df4ea6078
Andrea Tentner <tentner-andrea@norc.org>
2024-03-03 21:17:06 -0600

    got open access now flavor working

--- pkg_shareable_data.py
+++ pkg_shareable_data.py
@@ -27,22 +27,44 @@ def ignore_files_1(dir, files):
     else:
         return []
 
+# # defining the function to ignore the files 
+# # if present in any folder that does not start with "dsc-pkg"
+# # when folder starts with dsc-pkg do NOT ignore the files for copying
+# def ignore_files_2(dir, files, filesToKeep=[]):
+    
+#     dirStem = Path(dir)
+#     dirStem = dirStem.stem
+#     dirStemString = str(dirStem)
+
+#     #if not dirStemString.startswith("dsc-pkg"): # in this case subfolders in dsc pkg will not have files copied over
+#     if not "dsc-pkg" in dir: # in this case subfolders in dsc pkg should have files copied over
+#         filesToIgnore = [f for f in files if os.path.isfile(os.path.join(dir, f))]
+#         filesToIgnore = [f for f in filesToIgnore if os.path.join(dir, f) not in filesToKeep]
+#         return filesToIgnore
+#     else:
+#         return []
+
 # defining the function to ignore the files 
 # if present in any folder that does not start with "dsc-pkg"
 # when folder starts with dsc-pkg do NOT ignore the files for copying
-def ignore_files_2(dir, files, filesToKeep=[]):
+def ignore_files_2(filesToKeep):
+    def ignoref(dir, files):
     
-    dirStem = Path(dir)
-    dirStem = dirStem.stem
-    dirStemString = str(dirStem)
+        dirStem = Path(dir)
+        dirStem = dirStem.stem
+        dirStemString = str(dirStem)
 
-    #if not dirStemString.startswith("dsc-pkg"): # in this case subfolders in dsc pkg will not have files copied over
-    if not "dsc-pkg" in dir: # in this case subfolders in dsc pkg should have files copied over
-        filesToIgnore = [f for f in files if os.path.isfile(os.path.join(dir, f))]
-        filesToIgnore = [f for f in filesToIgnore if os.path.join(dir, f) not in filesToKeep]
-        return filesToIgnore
-    else:
-        return []
+        #if not dirStemString.startswith("dsc-pkg"): # in this case subfolders in dsc pkg will not have files copied over
+        if not "dsc-pkg" in dir: # in this case subfolders in dsc pkg should have files copied over
+            filesToIgnore = [f for f in files if os.path.isfile(os.path.join(dir, f))]
+            #filesToIgnore = set(filesToIgnore) - set(filesToKeep)
+            filesToIgnore = [f for f in filesToIgnore if Path(os.path.join(dir, f)) not in filesToKeep]
+            print("dir: ", dir)
+            print("filesToIgnore: ", filesToIgnore)
+            return filesToIgnore
+        else:
+            return ()
+    return ignoref
 
 def createShareableDataPkg(workingDataPkgDir,flavor="shell",shareableDataPkgShellDir="",StudyFolderCentralized=True,workingDataPkgDirInStudyFolder=True):
     # calling the shutil.copytree() method and
@@ -82,11 +104,12 @@ def createShareableDataPkg(workingDataPkgDir,flavor="shell",shareableDataPkgShel
                     trackerEntries["accessDateTimeStamp"] = pd.to_datetime(trackerEntries["accessDate"])
                     
                     trackerEntriesContainOpenAccess = trackerEntries[trackerEntries["access"].str.contains("open-access")]
+                    print("trackerEntriesContainOpenAccess: ",trackerEntriesContainOpenAccess)
                     
                     # get df with open-access resources that never had temporary-private access also set (should be open access now) 
-                    trackerEntriesContainOpenAccessAndNOTTempPrivate = trackerEntries[-trackerEntries["access"].str.contains("temporary-private")]
+                    trackerEntriesContainOpenAccessAndNOTTempPrivate = trackerEntriesContainOpenAccess[-trackerEntriesContainOpenAccess["access"].str.contains("temporary-private")]
                     # get df with open-access resources that ALSO had temporary-private access set (only open access if today is >= private date)
-                    trackerEntriesContainOpenAccessAndTempPrivate = trackerEntries[trackerEntries["access"].str.contains("temporary-private")]
+                    trackerEntriesContainOpenAccessAndTempPrivate = trackerEntriesContainOpenAccess[trackerEntriesContainOpenAccess["access"].str.contains("temporary-private")]
                     
                     # move this to the top so that all dfs have the private/access date timestamp column
                     # # get private date (access date) as a timestamp in order to be able to compare to today timestamp
@@ -101,10 +124,12 @@ def createShareableDataPkg(workingDataPkgDir,flavor="shell",shareableDataPkgShel
                     # 2) had temp private access assigned but today is >= the private/access date set by the user
                     trackerEntriesOpenAccessNow = pd.concat([trackerEntriesContainOpenAccessAndNOTTempPrivate,trackerEntriesContainOpenAccessAndTempPrivatePastPrivateDate], axis=0)
                     filesToKeep = trackerEntriesOpenAccessNow["path"].tolist()
+                    filesToKeep = [Path(f) for f in filesToKeep]
+                    print("filesToKeep: ",filesToKeep)
                     
                     shutil.copytree(str(studyFolderDirPath),
                                     shareablePkgDirString,
-                                    ignore=ignore_files_2(dir,files,filesToKeep=filesToKeep))
+                                    ignore=ignore_files_2(filesToKeep=filesToKeep))
 
 
                 # shutil.copytree(str(studyFolderDirPath),
@@ -114,5 +139,32 @@ def createShareableDataPkg(workingDataPkgDir,flavor="shell",shareableDataPkgShel
 
 #createShareableDataPkgShell(workingDataPkgDir="C:/Users/tentner-andrea/Documents/vivli-test-study/dsc-pkg/")
 
-createShareableDataPkg(workingDataPkgDir="P:/3652/Common/HEAL/y3-task-b-data-sharing-consult/repositories/vivli-submission-from-data-pkg/vivli-test-study/dsc-pkg/",flavor="shell")
+#createShareableDataPkg(workingDataPkgDir="P:/3652/Common/HEAL/y3-task-b-data-sharing-consult/repositories/vivli-submission-from-data-pkg/vivli-test-study/dsc-pkg/",flavor="shell")
 #createShareableDataPkg(workingDataPkgDir="P:/3652/Common/HEAL/y3-task-b-data-sharing-consult/repositories/vivli-submission-from-data-pkg/vivli-test-study/dsc-pkg/",flavor="open-access-now")
+
+# filesToKeep = [
+#     r"C:\Users\tentner-andrea\Documents\vivli-test-study\data\anonymized-analysis-dataset.dta",
+#     r"C:\Users\tentner-andrea\Documents\vivli-test-study\code\anonymize.do.txt",
+# ]
+
+# createShareableDataPkg(
+#     workingDataPkgDir="C:\\Users\\tentner-andrea\\Documents\\vivli-test-study\\dsc-pkg\\",
+#     flavor="shell")
+
+# createShareableDataPkg(
+#     workingDataPkgDir="C:\\Users\\tentner-andrea\\Documents\\vivli-test-study\\dsc-pkg\\",
+#     flavor="open-access-now")
+
+# createShareableDataPkg(
+#     workingDataPkgDir="P:/3652/Common/HEAL/y3-task-b-data-sharing-consult/repositories/vivli-submission-from-data-pkg/vivli-test-study/dsc-pkg/",
+#     flavor="shell")
+
+createShareableDataPkg(
+    workingDataPkgDir="P:/3652/Common/HEAL/y3-task-b-data-sharing-consult/repositories/vivli-submission-from-data-pkg/vivli-test-study/dsc-pkg/",
+    flavor="open-access-now")
+
+
+
+
+
+

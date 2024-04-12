add09f5b89bfada29b39eab482a6b775f245b523
Andrea Tentner <tentner-andrea@norc.org>
2024-04-12 09:49:39 -0500

    more work on programmatic update of template readme for upload of shareable data pkg(s)

--- readme.py
+++ readme.py
@@ -3,17 +3,28 @@ import yaml
 with open("./readme.yaml","r") as file:
     contents = yaml.safe_load(file)
 
-print(contents)
+pkgName = "example-shareable-data-package-name-2"
+
+#print(contents)
 contents["Get-started"]["Description"] = "hello"
 
 pkgList = contents["Get-started"]["Contents"]["shareable-data-packages"]
-addPkg = pkgList[0]
-print(addPkg)
-addPkg["shareable-data-package"]["package-name"] = "blahblah"
+print("pkgList: ",pkgList)
+pkgListCopy = pkgList.copy()
+print("pkgListCopy: ",pkgList)
+addPkg = pkgListCopy[0].copy()
+addPkg[pkgName] = addPkg.pop("example-shareable-data-package-name-1")
+print("addPkg: ", addPkg)
+# addPkg["shareable-data-package"]["package-name"] = "blahblah"
+# print("addPkg: ", addPkg)
 pkgList.append(addPkg)
-contents["Get-started"]["Contents"]["shareable-data-packages"] = pkgList 
-
-print(contents)
+print("pkgList: ",pkgList)
+# contents["Get-started"]["Contents"]["shareable-data-packages"] = pkgList 
+# print("pkgList: ",pkgList)
+# print(contents)
+# print("pkgList second element: ",pkgList[1])
+# pkgList[1]["shareable-data-package"]["package-name"] = "blahblah"
+# print("pkgList: ",pkgList)
 
 
 
